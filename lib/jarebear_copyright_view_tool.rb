915f3e046647add14240095e52125fcd1293fe06
Jared Blake <jaredmarshallblake@gmail.com>
2020-03-29 20:24:07 -0400

    initial commit

---
+++ lib/jarebear_copyright_view_tool.rb
@@ -0,0 +1,6 @@
+require "jarebear_copyright_view_tool/version"
+require "jarebear_copyright_view_tool/renderer"
+
+module JarebearCopyrightViewTool
+  # Your code goes here...
+end
---
+++ lib/jarebear_copyright_view_tool/version.rb
@@ -0,0 +1,3 @@
+module JarebearCopyrightViewTool
+  VERSION = "0.1.0"
+end
